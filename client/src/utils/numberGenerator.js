26d1e59d823fc6c34f2277150916fe86cda40cce
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2024-09-15 16:30:13 -0500

    appendix added, number generator fixed

--- client/src/utils/numberGenerator.js
+++ client/src/utils/numberGenerator.js
@@ -1,51 +1,57 @@
 const EFFORT_LEVELS = {
-  1: '1',
-  2: '2',
-  3: '3',
-  4: '4'
+  1: '1-2 switches',
+  2: '3-4 switches',
+  3: '5-6 switches',
+  4: '7+ switches'
 };
 
-const generateDigit = (isOdd) => {
-  return isOdd ? Math.floor(Math.random() * 5) * 2 + 1 : Math.floor(Math.random() * 5) * 2;
+const generateDigit = () => Math.floor(Math.random() * 10);
+
+const countSwitches = (number) => {
+  let switches = 0;
+  for (let i = 1; i < number.length; i++) {
+    if ((number[i] % 2) !== (number[i-1] % 2)) {
+      switches++;
+    }
+  }
+  return switches;
 };
 
 const generateNumber = (effortLevel) => {
-  let number = '';
-  for (let i = 0; i < 9; i++) {
-    switch (effortLevel) {
-      case '1':
-        number += generateDigit(i % 2 === 0);
-        break;
-      case '2':
-        number += generateDigit(i % 2 === 1);
-        break;
-      case '3':
-        number += generateDigit(Math.random() < 0.5);
-        break;
-      case '4':
-        const pattern = [0, 1, 1, 0, 0, 1, 0, 1, 0];
-        number += generateDigit(pattern[i] === 1);
-        break;
-    }
+  let number;
+  let switches;
+  do {
+    number = Array(9).fill().map(() => generateDigit()).join('');
+    switches = countSwitches(number);
+  } while (!isValidForLevel(switches, effortLevel));
+  
+  return { number, effortLevel };
+};
+
+const isValidForLevel = (switches, level) => {
+  switch (level) {
+    case '1': return switches >= 1 && switches <= 2;
+    case '2': return switches >= 3 && switches <= 4;
+    case '3': return switches >= 5 && switches <= 6;
+    case '4': return switches >= 7;
+    default: return false;
   }
-  return { number: number.toString(), effortLevel };
-};export const generateTrialNumbers = () => {
+};
+
+export const generateTrialNumbers = () => {
   const trialNumbers = [];
-  const effortLevels = Object.values(EFFORT_LEVELS);
+  const effortLevels = Object.keys(EFFORT_LEVELS);
   
-  // Create an array with 5 of each effort level
-  const balancedLevels = effortLevels.flatMap(level => Array(5).fill(level));
-  
-  // Fisher-Yates shuffle
-  for (let i = balancedLevels.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [balancedLevels[i], balancedLevels[j]] = [balancedLevels[j], balancedLevels[i]];
+  for (let i = 0; i < 20; i++) {
+    const level = effortLevels[Math.floor(i / 5) % 4];
+    trialNumbers.push(generateNumber(level));
   }
   
-  // Generate numbers based on the shuffled levels
-  balancedLevels.forEach(level => {
-    trialNumbers.push(generateNumber(level));
-  });
+  // Shuffle the trial numbers
+  for (let i = trialNumbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [trialNumbers[i], trialNumbers[j]] = [trialNumbers[j], trialNumbers[i]];
+  }
   
   return trialNumbers;
 };
